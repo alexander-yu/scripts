7664218c95106db38226ec6bb75495765bc7d858
Alex Yu <yu.alex96@gmail.com>
2020-09-15 01:26:21 -0400

    Add spotify client

--- spotify/client.py
+++ spotify/client.py
@@ -1,3 +1,32 @@
 import configparser
 
+import cachetools
 import spotipy
+import spotipy.oauth2
+
+
+CACHE_PATH = '.cache'
+REDIRECT_URI = 'https://localhost:5000/callback'
+SCOPES = ' '.join([
+    'user-library-modify',
+    'user-library-read',
+])
+
+
+@cachetools.cached(cachetools.Cache(1))
+def spotify_client() -> spotipy.Spotify:
+    config = configparser.ConfigParser()
+    config.read('secrets.ini')
+    client_id = config['client']['id']
+    client_secret = config['client']['secret']
+
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPES,
+        cache_path=CACHE_PATH,
+        # TODO (ayu): use open_browser=False and go browserless when spotipy is next updated
+    )
+
+    return spotipy.Spotify(auth_manager=auth_manager)
