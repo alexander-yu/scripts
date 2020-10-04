d3453ff46b01802645765061e9eb412b3a67ac6b
Alex Yu <yu.alex96@gmail.com>
2020-10-04 00:23:08 -0400

    Add get_album

---
+++ spotify/get_album.py
@@ -0,0 +1,25 @@
+import pprint
+
+import click
+import spotipy
+
+import client
+
+@click.command()
+@click.argument('album_id')
+@click.option('--include_tracks', default=False, help='Whether or not to include tracks in the output')
+@click.option('--include_markets', default=False, help='Whether or not to include markets in the output')
+def run(album_id, include_tracks, include_markets):
+    spotify = client.spotify_client()
+    album = spotify.album(album_id)
+
+    if not include_tracks:
+        album.pop('tracks')
+    if not include_markets:
+        album.pop('available_markets')
+
+    pprint.pprint(album)
+
+
+if __name__ == '__main__':
+    run()
